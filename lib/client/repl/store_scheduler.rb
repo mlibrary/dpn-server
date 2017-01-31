a2a6e3336dc650f6a73084b2b52df900e2ced5a9
Bryan Hockey <bhock@umich.edu>
2017-01-30 23:22:16 -0600

    StoreScheduler now works from flows instead of replications

--- lib/client/repl/store_scheduler.rb
+++ lib/client/repl/store_scheduler.rb
@@ -7,37 +7,35 @@
 module Client
 
   class StoreScheduler
-
     def schedule
-      replications.each do |replication|
-        flow = ReplicationFlow.find_by_replication_id(replication.replication_id)
-        if should_store?(flow)
-          store_attempt = flow.store_attempts.create!(start_time: Time.now.utc)
-          StoreJob.perform_later(store_attempt)
+      flows.each do |flow|
+        if replications.include? flow.replication_id
+          attempt = flow.store_attempts.create!(start_time: Time.now.utc)
+          StoreJob.perform_later(attempt)
         end
       end
     end
 
-
-    def should_store?(flow)
-      !flow.stored? &&
-        !flow.store_ongoing?
-        !flow.store_attempts.count < 5
+    def flows
+      ReplicationFlow
+        .retrieved
+        .unpacked
+        .validated
+        .fixity_complete
+        .received_notified
+        .not.stored
+        .not.store_ongoing
     end
 
-
     def replications
-      # We define the query here instead of on the model to facilitate
-      # moving this functionality to a standalone project.
-      ReplicationTransfer
+      @replications ||= Set.new(ReplicationTransfer
         .where(to_node: Node.local_node!)
         .where(cancelled: false)
         .where(stored: false)
         .where(store_requested: true)
         .where.not(fixity_value: nil)
-        .includes(:from_node, :bag)
+        .pluck(:replication_id))
     end
-
   end
 
 end
