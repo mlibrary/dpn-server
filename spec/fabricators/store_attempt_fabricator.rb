e35d1aaacb6e7de8c815fcb043ba14f3b2834698
Bryan Hockey <bhock@umich.edu>
2017-01-31 13:36:56 -0600

    StoreAttempt unit tests

---
+++ spec/fabricators/store_attempt_fabricator.rb
@@ -0,0 +1,13 @@
+# Copyright (c) 2015 The Regents of the University of Michigan.
+# All Rights Reserved.
+# Licensed according to the terms of the Revised BSD License
+# See LICENSE.md for details.
+
+
+Fabricator(:store_attempt) do
+  replication_flow { Fabricate(:replication_flow) }
+  start_time { 2.hours.ago }
+  end_time nil
+  success nil
+  error nil
+end
---
+++ spec/models/store_attempt_spec.rb
@@ -0,0 +1,15 @@
+# Copyright (c) 2015 The Regents of the University of Michigan.
+# All Rights Reserved.
+# Licensed according to the terms of the Revised BSD License
+# See LICENSE.md for details.
+
+
+require 'rails_helper'
+
+describe StoreAttempt do
+  it_behaves_like "an attempt", [:bag, :unpacked_location] do
+    let(:fabricator) { :store_attempt }
+    let(:success_args) { [] }
+    let(:failure_args) { [] }
+  end
+end
