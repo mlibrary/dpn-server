b9e713a3edce5e5360d24a894dafb2bed273438f
Bryan Hockey <bhock@umich.edu>
2018-03-16 17:40:39 -0500

    Actually get the config to run
    This commit includes the use of the lonely operator (&.). We do
    this because even though the production configuration is not used
    in dev or test, the ERB is still parsed.
    
    This problem is unavoidable with database.yml. It may be unavoidable
    with secrets.yml. It should be avoidable with all other configuration
    files. From there, we can just supply the sensible defaults for the
    non-production environments via ettin in config/settings.

--- config/initializers/dpn.rb
+++ config/initializers/dpn.rb
@@ -3,10 +3,8 @@
 # Licensed according to the terms of the Revised BSD License
 # See LICENSE.md for details.
 
-config_file = File.join Rails.root, "config", "dpn.yml"
-cfg = YAML.load(ERB.new(IO.read(config_file)).result)[Rails.env]
-
 Rails.application.configure do
+  cfg = config_for(:dpn)
   config.local_namespace  = cfg["local_namespace"]
   config.local_api_root   = cfg["local_api_root"]
   config.staging_dir      = cfg["staging_dir"]
