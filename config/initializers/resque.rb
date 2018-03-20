mlibrary/dpn-server

commit b9e713a3edce5e5360d24a894dafb2bed273438f

Actually get the config to run
This commit includes the use of the lonely operator (&.). We do
this because even though the production configuration is not used
in dev or test, the ERB is still parsed.

This problem is unavoidable with database.yml. It may be unavoidable
with secrets.yml. It should be avoidable with all other configuration
files. From there, we can just supply the sensible defaults for the
non-production environments via ettin in config/settings.

config/initializers/resque.rb
@@ -24,7 +24,7 @@ module ResqueInit
       rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
       rails_env = ENV['RAILS_ENV'] || 'development'
 
-      resque_config = YAML.load_file(File.join rails_root, '/config/resque.yml')
+      resque_config = YAML.load(ERB.new(File.join(rails_root, '/config/resque.yml')).result)
       Resque.redis = resque_config[rails_env]
       Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
       Resque.inline = Rails.env.test?

config/database.yml
@@ -25,4 +25,4 @@ test:
 # Rails is insane, and still tries to resolve these values.
 # We fix this by using the lonely operator.
 production:
-  url: <%= Rails.env.production? ? DPN.config.db&.url : "" %>
+  url: <%= Rails.env.production? ? DPN.config.db.url : "" %>

config/dpn.yml
@@ -37,5 +37,12 @@ test:
   repo_dir: <%= File.join "/tmp", "dpnrepo", "preservation" %>
   queue_adapter: :test
 
-production: <%= DPN.config.dpn %>
+# We call to_h here for now because YAML converts Ettin::Options
+# objects straight to nil, silently.
+production:
+  local_namespace: <%= DPN.config.dpn&.local_namespace %>
+  local_api_root: <%= DPN.config.dpn&.local_api_root %>
+  staging_dir: <%= DPN.config.dpn&.staging_dir %>
+  repo_dir: <%= DPN.config.dpn&.repo_dir %>
+  queue_adapter: <%= DPN.config.dpn&.queue_adapter %>
 

config/initializers/dpn.rb
@@ -3,10 +3,8 @@
 # Licensed according to the terms of the Revised BSD License
 # See LICENSE.md for details.
 
-config_file = File.join Rails.root, "config", "dpn.yml"
-cfg = YAML.load(ERB.new(IO.read(config_file)).result)[Rails.env]
-
 Rails.application.configure do
+  cfg = config_for(:dpn)
   config.local_namespace  = cfg["local_namespace"]
   config.local_api_root   = cfg["local_api_root"]
   config.staging_dir      = cfg["staging_dir"]

config/resque-pool.yml
@@ -5,5 +5,5 @@ development: &development
 test:
   <<: *development
 
-production: <%= DPN.config.resque_pool %>
+production: <%= DPN.config.resque_pool.to_h %>
 

config/secrets.yml
@@ -22,8 +22,8 @@ test:
   secret_key_base: 525e7a0e528c0972699d038ef6a5eb35c493cd6db6db85baf0bf756ba59aeb50a2f5bd38b421d61cc99683df3da779048ec615a5fd72e62c65a86f3f71edcc25
 
 production:
-  secret_key_base: <%= DPN.config.secret_key_base %>
-  salt: <%= DPN.config.salt %>
-  cipher_key: <%= DPN.config.cipher_key %>
-  cipher_iv: <%= DPN.config.cipher_iv %>
-  transfer_private_key: <%= DPN.config.transfer_private_key %>
+  secret_key_base: <%= DPN.config.secrets&.secret_key_base %>
+  salt: <%= DPN.config.secrets&.salt %>
+  cipher_key: <%= DPN.config.secrets&.cipher_key %>
+  cipher_iv: <%= DPN.config.secrets&.cipher_iv %>
+  transfer_private_key: <%= DPN.config.secrets&.transfer_private_key %>

commit 68619cc26835ceb029cd8e1df93172444f1387aa

Fix bug where we did not read resque.yml
This was introduced in b9e713a3 when we added ERB support.
The YAML.load_file call was changed to YAML.load, without
a subsequent addition of File.read. This commit adds File.read.

config/initializers/resque.rb
@@ -24,7 +24,7 @@ module ResqueInit
       rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
       rails_env = ENV['RAILS_ENV'] || 'development'
 
-      resque_config = YAML.load(ERB.new(File.join(rails_root, '/config/resque.yml')).result)
+      resque_config = YAML.load(ERB.new(File.read(File.join(rails_root, '/config/resque.yml'))).result)
       Resque.redis = resque_config[rails_env]
       Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
       Resque.inline = Rails.env.test?
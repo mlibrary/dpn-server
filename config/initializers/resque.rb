68619cc26835ceb029cd8e1df93172444f1387aa
Bryan Hockey <bhock@umich.edu>
2018-03-20 16:32:14 -0500

    Fix bug where we did not read resque.yml
    This was introduced in b9e713a3 when we added ERB support.
    The YAML.load_file call was changed to YAML.load, without
    a subsequent addition of File.read. This commit adds File.read.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -24,7 +24,7 @@ module ResqueInit
       rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
       rails_env = ENV['RAILS_ENV'] || 'development'
 
-      resque_config = YAML.load(ERB.new(File.join(rails_root, '/config/resque.yml')).result)
+      resque_config = YAML.load(ERB.new(File.read(File.join(rails_root, '/config/resque.yml'))).result)
       Resque.redis = resque_config[rails_env]
       Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
       Resque.inline = Rails.env.test?
